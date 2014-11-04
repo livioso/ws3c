5209155ef813180c0c3a215367e6d5d7405693a7
Livio Bieri <livio@livio.li>
2014-11-04 23:48:58 +0100

    Checking out Nebula theme.

--- presentation/src/scripts/main.js
+++ presentation/src/scripts/main.js
@@ -1,18 +1,21 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
   cube = require('bespoke-theme-cube'),
+  nebula = require('bespoke-theme-nebula'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
   scale = require('bespoke-scale'),
-  progress = require('bespoke-progress');
+  progress = require('bespoke-progress'),
+  backdrop = require('bespoke-backdrop');;
 
 // Bespoke.js
 bespoke.from('article', [
-  cube(),
+  nebula(),
   keys(),
   touch(),
   bullets('li, .bullet'),
   scale(),
-  progress()
+  progress(),
+  backdrop()
 ]);
